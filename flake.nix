64e7edeb119a467cfa0a1a8d9825806216fec716
banst <bastien@loftorbital.com>
2025-01-24 17:58:16 +0100

    feat: initialise repository

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Clef flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+  };
+  
+  
+  outputs = { self, nixpkgs, utils, gomod2nix }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ gomod2nix.overlays.default ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            name = "b4nst/clef";
+            buildInputs = [
+              go
+              golangci-lint
+              gopls
+              gotools
+            ];
+         };
+        }
+      );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/b4nst/clef
+
+go 1.23.4
